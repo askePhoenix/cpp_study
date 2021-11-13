62ce37110cee1e2747bab11550edfb8542aad0d7
askePhoenix <aistarteverything@gmail.com>
2021-11-14 00:54:56 +0900

    1강 #2
    ## 배운 내용
    - 반복문

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
-// chap.1-5 분기문
+// chap.1-6 반복문
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    int number;
-    cin >> number;
 
-    if (number % 2 == 0) {
-        cout << to_string(number) + "는 짝수입니다." << endl;
+    for (int i = 0; i < 10; ++i) {
+        cout << i + 1 << "입니다." << endl;
     }
+    // 별찍기 / 정사각형 / 직삼각형 / 이등변 삼각형
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < i + 1; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 4 - i; ++j) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
