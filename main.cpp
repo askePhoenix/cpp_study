7eda4e29e954e910035da4b8796696071d980307
askePhoenix <aistarteverything@gmail.com>
2021-11-14 00:39:57 +0900

    기초 + 1강 #2
    ## 배운 내용
    - 주석 및 네임스페이스
    - 함수 : 간단한 포맷

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+// chap.1 콘솔 화면에 출력하는 프로그램 작성
 #include <iostream>
-
+// namespace : 이름 공간, 풀 네임에서 '성'과 '이름' 중 '성'을 의미합니다.
+// namespace 로 중복되는 함수 호출을 하지 않도록 합니다.
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // cout : iostream 에 정의됨.
+    // console out 의 약자 입니다. : 콘솔에 해당 값을 출력합니다.
+    cout << "Hello, World!" << endl;
     return 0;
 }
