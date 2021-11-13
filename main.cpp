dfedf017ee786ec65634fd5895ec24da22e90a04
askePhoenix <aistarteverything@gmail.com>
2021-11-14 01:11:57 +0900

    2강 #3
    ## 배운 내용
    - 함수의 선언 및 사용법

--- main.cpp
+++ main.cpp
@@ -1,37 +1,16 @@
-// chap.1-6 반복문
+// chap.2 함수 사용하기
 #include <iostream>
 
 using namespace std;
 
+int plus1(int x);
+
 int main() {
-
-    for (int i = 0; i < 10; ++i) {
-        cout << i + 1 << "입니다." << endl;
-    }
-    // 별찍기 / 정사각형 / 직삼각형 / 이등변 삼각형
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < i + 1; ++j) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 4 - i; ++j) {
-            cout << " ";
-        }
-        for (int j = 0; j < 2 * i + 1; ++j) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
+    int k = plus1(99);
+    cout << k;
     return 0;
+}
+
+int plus1(int x){
+    return x+1;
 }
