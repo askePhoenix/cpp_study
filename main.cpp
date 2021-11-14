8850deccb665e862798c369db437c4fa9f340b98
askePhoenix <aistarteverything@gmail.com>
2021-11-14 15:39:15 +0900

    4강 #8
    ## 배운 내용
    - Character 클래스 만들기

--- main.cpp
+++ main.cpp
@@ -1,15 +1,89 @@
-// chap.4-3 포인터 -> swap
+// 심화 객체 접근 한정자
 #include <iostream>
 #include "common.h"
+
 using namespace std;
 
+class Character {
+public:
+    int getHp() const;
+
+    void setHp(int hp);
+
+    int getSpeed() const;
+
+    void setSpeed(int speed);
+
+    const string &getName() const;
+
+    void setName(const string &name);
+
+private:
+    int hp{};
+    int speed{};
+    string name;
+
+public:
+
+    void move(int dir);
+    void jump();
+    void hit();
+
+    void speedUp();
+    void obtainItem();
+};
+void Character::speedUp() {
+    speed++;
+}
+void Character::obtainItem() {
+    speedUp();
+}
+
+int Character::getHp() const {
+    return hp;
+}
+
+void Character::setHp(int hp) {
+    Character::hp = hp;
+}
+
+int Character::getSpeed() const {
+    return speed;
+}
+
+void Character::setSpeed(int speed) {
+    Character::speed = speed;
+}
+
+const string &Character::getName() const {
+    return name;
+}
+
+void Character::setName(const string &name) {
+    Character::name = name;
+}
+
+void Character::jump() {
+
+}
+
+void Character::move(int dir) {
+
+}
+
+void Character::hit() {
+
+}
 
 int main() {
-    int x = 999;
-    int y = 1313;
+    Character mario;
+    mario.setName("super Mario");
+    mario.setHp(100);
+    mario.obtainItem();
 
-    swap(&x, &y);
-    cout << x << " / " << y << endl;
+    cout << mario.getHp() << endl;
+    cout << mario.getName() << endl;
+
 
     return 0;
 }
